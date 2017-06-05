5b9d6e09086a16e7b96404c372273f16dbc61f91
Alena Golovina and Anna Timofeeva <alena@>
2017-06-05 16:08:24 -0700

    add user input fields, looping for displaying output

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
-  
+  $("#submit_button").click(function(event) {
+  var items = ["item1", "item2", "item3", "item4", "item5"];
+  items.forEach(function(item) {
+    var userInput = $("input#" + item).val();
+
+
+
+  $("ul").append("<li>"+ userInput + "</li>");
 });
+});
+});
+
+
+
+/*  var male1 = $("input#actor1").val();
+  var male2 = $("input#actor2").val();
+  var male3 = $("input#actor3").val();
+  var male4 = $("input#actor4").val();
+  var male5 = $("input#actor4").val();
+/*$("div#output").text(male1 + ", " + male2 + ", " + male3 + ", " + male4 + ", " + male5);*/
+
+/*var maleInput = [male1, male2, male3, male4, male5];
+var maleNew = [];
+maleNew.push(maleInput[1]);
+maleNew.push(maleInput[0]);
+maleNew.push(maleInput[2]);
+*/
+
+/*$("div#output").text(maleInput);
+  /*var female1 = $("input#actor1").val();
+  $("div#output").text(female1);
+  var female2 = $("input#actor1").val();
+  $("div#output").text(female2);
+  var female3 = $("input#actor1").val();
+  $("div#output").text(female3);
+  var female4 = $("input#actor1").val();
+  $("div#output").text(female4);
+  var female5 = $("input#actor1").val();
+  $("div#output").text(female5);
+  */
+
+//alert(maleNew);
+/*$("ul").append("<li>"+ maleNew[0] + "</li>");
+$("ul").append("<li>"+ maleNew[1] + "</li>");
+$("ul").append("<li>"+ maleNew[2] + "</li>");
+
+  });
+});
+*/
